edc76e3a1bd0746093b8b9649c464ff041979807
Cleber Junior <CleberJr@Clebers-MacBook-Pro.local>
2018-09-23 12:23:47 -0300

    ReactNativeApp

--- src/index.js
+++ src/index.js
@@ -1,60 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 import 'config/ReactotronConfig';
 import 'config/DevToolsConfig';
 
-import Todo from 'components/Todo';
 
 export default class App extends Component {
-  
-  state = {
-    todos: [
-      { id: 0, text: 'Read a Arthicle' },
-      { id: 1, text: 'Study React Native' },
-    ]
-  }
-
-  addTodo = () => {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        { id: Math.random(), text: 'New To do' },
-      ]
-    })
-  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Todo title={'Do Coffee'}/>
-        <Todo title={'Study React Native'}/>
-      </View>
-    )
+    return true;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    color: '#000',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  box : {
-    width: 80,
-    height: 80,
-    backgroundColor: '#F00',
-    margin: 10,
-    transform: [
-      { rotateZ: '20deg' }, 
-    ]
-  },
-
-  boxText: {
-    color: '#FFF',
-  },
-});
